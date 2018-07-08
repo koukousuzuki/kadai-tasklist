aa4036f5a5835790b11091d7da66461a748e8a1d
koudai.suzuki <koudai6456@icloud.com>
2018-07-08 14:11:41 +0000

    Bootstrap

--- db/migrate/20180609154753_create_tasks.rb
+++ db/migrate/20180609154753_create_tasks.rb
@@ -1,10 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
-
+      t.string :content
+      
       t.timestamps
     end
   end
