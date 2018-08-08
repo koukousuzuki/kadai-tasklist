2fec77f9c37f92d926a17b92781afd0dfc1aaef7
koudai.suzuki <koudai6456@icloud.com>
2018-08-08 14:21:23 +0000

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:index,:show,:create]
-  before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
     if @current_user
