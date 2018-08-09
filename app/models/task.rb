e2fe7b73e6d73fee389a48c76582191a08979034
koudai.suzuki <koudai6456@icloud.com>
2018-08-09 08:17:42 +0000

    routes

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ApplicationRecord
   
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+  validates :status, presence: true, length: { maximum: 50 }
+
 end
